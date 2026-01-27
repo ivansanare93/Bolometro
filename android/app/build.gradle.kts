19ced14e4f2222c357d7b288249e8d50c35e9eba
ivansanare93 <ivansanare93@gmail.com>
2026-01-27 12:13:22 +0100

    cambio version ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 android {
     namespace = "com.bolometro"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
